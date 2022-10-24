240cc140cd5b90c086d8f757fceeb9fad10d17d7
Heleno Nascimento <heleno_55@hotmail.com>
2022-10-24 12:02:19 -0300

    adicionado componente botao e indexado os icones

---
+++ src/data/items.ts
@@ -0,0 +1,15 @@
+import android from '../svgs/android.svg'
+import caminhao from '../svgs/caminhao.svg';
+import disney from '../svgs/disney.svg';
+import estrela from '../svg/estrela.svg';
+import gasolina from '../svg/gasolina.svg';
+import moto from '../svg/moto.svg';
+
+export const items = [
+    {name: 'android', icon: android},
+    {name: 'caminhao', icon: caminhao},
+    {name: 'disney', icon: disney},
+    {name: 'estrela', icon: estrela},
+    {name: 'gasolina', icon: gasolina},
+    {name: 'moto', icon: moto},
+];
